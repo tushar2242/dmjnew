4ffa8b93e35d7bc086d7a3922be2412c9b965a44
Himanshi Sharma <himanshi.sharma.ahit@gmail.com>
2023-10-03 18:14:59 +0530

    v9.5

--- src/jsx/checkout/CheckoutPage.jsx
+++ src/jsx/checkout/CheckoutPage.jsx
@@ -20,7 +20,7 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import creditcard from "./images/credit.png";
 import paypal from "./images/paypal.png";
-import upilogo from '../../assets/images/bhim.png';
+import upilogo from "../../assets/images/bhim.png";
 import mastercard from "./images/mastercard.png";
 import phonpe from "./images/phonpe.png";
 import paytm from "./images/paytm.png";
@@ -31,72 +31,60 @@ import { useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import Accordion from "react-bootstrap/Accordion";
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from "@mui/icons-material/Add";
+import { Select } from "@mui/material";
+
 
 const url = "https://api.diwamjewels.com/DMJ/";
 const imgUrl = "https://squid-app-2-7wbvi.ondigitalocean.app/";
 // const endPoint = 'api/v1/products/';
 const productEndPoint = "api/v1/products";
-const endPoint = 'api/v1/user/';
+const endPoint = "api/v1/user/";
 
-const userId = localStorage.getItem('userId')
-
+const userId = localStorage.getItem("userId");
 
 var cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 const CheckoutPage = () => {
-  const [delivery, setDelivery] = useState([
-    {
-      fName: "",
-      lName: "",
-      mobileNo: "",
-      address: "",
-      address2: "",
-      city: "",
-      country: "",
-      postalCode: "",
-    },
-  ]);
 
-
-
-  const [userInfo, setUserInfo] = useState(false)
+  const [userInfo, setUserInfo] = useState(false);
 
   async function fetchUserData() {
     try {
-      const res = await axios.get(url + endPoint + userId)
+      const res = await axios.get(url + endPoint + userId);
       // console.log(res.data.data)
-      setUserInfo(res.data.data)
-    }
-    catch (err) {
-      console.log(err)
+      setUserInfo(res.data.data);
+    } catch (err) {
+      console.log(err);
     }
   }
 
-
   useEffect(() => {
-    fetchUserData()
-  }, [])
+    fetchUserData();
+  }, []);
 
   return (
     <>
       <HeaderCon />
       <Navbar />
+    
       <div className="checkout-bg">
         <div className="container-fluid">
-
           <div className="row fl-dirt-col">
             <div className="col-md-7 mt-3">
               <DeliveryCountry />
               <PromoCode />
-              <EmailAddress email={userInfo ? userInfo.email : 'Login For Buy Now'} />
+              <EmailAddress
+                email={userInfo ? userInfo.email : "Login For Buy Now"}
+              />
               <Accordion defaultActiveKey="0" className="mt-2">
                 <Accordion.Item eventKey="0">
                   <Accordion.Header className="hd-tag-font">
                     Billing Address
                   </Accordion.Header>
                   <Accordion.Body>
-                    <DlryAddress delivery={delivery} setDelivery={setDelivery} />
+                    <DlryAddress
+                    />
                   </Accordion.Body>
                 </Accordion.Item>
               </Accordion>
@@ -106,22 +94,22 @@ const CheckoutPage = () => {
                     Shipping Address
                   </Accordion.Header>
                   <Accordion.Body>
-                    <DlryAddress delivery={delivery} setDelivery={setDelivery} />
+                    <DlryAddress 
+                    />
                   </Accordion.Body>
                 </Accordion.Item>
               </Accordion>
 
-
-              <DlryOptions delivery={delivery} />
+              <DlryOptions />
 
               <PaymentType />
 
               <Button className="ck-buy-btn">BUY NOW</Button>
               <p className="cond-font">
-                By placing your order you agree to our Terms & Conditions, privacy
-                and returns policies . You also consent to some of your data being
-                stored by , which may be used to make future shopping experiences
-                better for you.
+                By placing your order you agree to our Terms & Conditions,
+                privacy and returns policies . You also consent to some of your
+                data being stored by , which may be used to make future shopping
+                experiences better for you.
               </p>
             </div>
 
@@ -232,14 +220,43 @@ const EmailAddress = ({ email }) => {
 };
 
 const DlryAddress = (props) => {
-  const { delivery, setDelivery } = props;
+  // const { delivery, setDelivery } = props;
+  const [selectedCountry, setSelectedCountry] = useState('India');
+  const [delivery, setDelivery] = useState([
+    {
+      fName: "",
+      lName: "",
+      mobileNo: "",
+      address: "",
+      address2: "",
+      city: "",
+      state:"",
+      postalCode: "",
+    },
+  ]);
 
+  const [isHomeChecked, setIsHomeChecked] = useState(false);
+  const [isOfficeChecked, setIsOfficeChecked] = useState(false);
+
+  const handleCheckboxChange = (e) => {
+    if (e.target.name === 'home') {
+      setIsHomeChecked(!isHomeChecked);
+      setIsOfficeChecked(false); // Uncheck "Office"
+    } else if (e.target.name === 'office') {
+      setIsOfficeChecked(!isOfficeChecked);
+      setIsHomeChecked(false); // Uncheck "Home"
+    }
+  };
   return (
     <>
       <div className="del-ct-bg mt-2">
-      <h3 className="hd-tag-font text-end" style={{color:'#b79d33',cursor:'pointer'}}><AddIcon /> ADD NEW ADDRESS</h3>
+        <h3
+          className="hd-tag-font text-end"
+          style={{ color: "#b79d33", cursor: "pointer" }}
+        >
+          <AddIcon /> ADD NEW ADDRESS
+        </h3>
         <h3 className="hd-tag-font">DELIVERY ADDRESS</h3>
-      
 
         <FormLabel className="fm-lbl-heading">Full NAME </FormLabel>
         <br />
@@ -248,10 +265,7 @@ const DlryAddress = (props) => {
           className="input-box-bdr mt-1"
           required
           value={delivery.fName}
-          onChange={(e) => {
-            setDelivery((prev) => ({ ...prev, fName: e.target.value }));
-            
-          }}
+          onChange={(e) => setDelivery({ ...delivery, fName: e.target.value })}
         ></Input>
         <br />
 
@@ -262,10 +276,7 @@ const DlryAddress = (props) => {
           className="input-box-bdr mt-1"
           required
           value={delivery.mobileNo}
-          onChange={(e) => {
-            setDelivery((prev) => ({ ...prev, mobileNo: e.target.value }));
-          
-          }}
+          onChange={(e) => setDelivery({ ...delivery, mobileNo: e.target.value })}
         ></Input>
         <br />
         <FormLabel className="fm-lbl-heading">POSTAL CODE </FormLabel>
@@ -274,9 +285,7 @@ const DlryAddress = (props) => {
           type="number"
           className="input-box-bdr mt-1"
           value={delivery.postalCode}
-          onChange={(e) => {
-            setDelivery((prev) => ({ ...prev, postalCode: e.target.value }));
-          }}
+          onChange={(e) => setDelivery({ ...delivery, postalCode: e.target.value })}
         ></Input>
         <br />
         <FormLabel className="fm-lbl-heading">ADDRESS </FormLabel>
@@ -286,10 +295,7 @@ const DlryAddress = (props) => {
           className="input-box-bdr mt-1"
           required
           value={delivery.address}
-          onChange={(e) => {
-            setDelivery((prev) => ({ ...prev, address: e.target.value }));
-        
-          }}
+          onChange={(e) => setDelivery({ ...delivery, address: e.target.value })}
         ></Input>
         <br />
         <Input
@@ -297,10 +303,7 @@ const DlryAddress = (props) => {
           className="input-box-bdr mt-1"
           required
           value={delivery.address2}
-          onChange={(e) => {
-            setDelivery((prev) => ({ ...prev, address2: e.target.value }));
-        
-          }}
+          onChange={(e) => setDelivery({ ...delivery, address2: e.target.value })}
         ></Input>
         <br />
 
@@ -311,40 +314,51 @@ const DlryAddress = (props) => {
           className="input-box-bdr mt-1"
           required
           value={delivery.city}
-          onChange={(e) => {
-            setDelivery((prev) => ({ ...prev, city: e.target.value }));
-      
-          }}
+          onChange={(e) => setDelivery({ ...delivery, city: e.target.value })}
         ></Input>
         <br />
         <FormLabel className="fm-lbl-heading">STATE </FormLabel>
         <br />
-        <Input
-          type="text"
-          className="input-box-bdr mt-1"
-          required
-        ></Input>
+        <Input type="text" className="input-box-bdr mt-1" required 
+        value={delivery.state} 
+        onChange={(e) => setDelivery({ ...delivery, state: e.target.value })}>
+
+        </Input>
         <br />
 
         <FormLabel className="fm-lbl-heading">COUNTRY</FormLabel>
         <br />
-        <Input
-          type="text"
-          className="input-box-bdr mt-1"
-          required
-          value={delivery.country}
-          onChange={(e) => {
-            setDelivery((prev) => ({ ...prev, country: e.target.value }));
-            
-          }}
-        ></Input>
+        <select className="input-box-bdr mt-1" required
+         value={selectedCountry}
+         onChange={e => setSelectedCountry(e.target.value)}
+         >
+        <option value="India">India</option>
+        <option value="USA">USA</option>
+        </select>
         <br />
-          
-          <div className="d-flex">
-          <button className="dlry-work-btn">Home</button>
-          <button className="dlry-work-btn">Office</button>
-          </div>
-    
+
+        <div className="d-flex">
+        <label className="dlry-work-btn">
+            <input
+              type="checkbox"
+              name="home"
+              checked={isHomeChecked}
+              onChange={handleCheckboxChange}
+             
+            />
+            Home
+          </label>
+          <label className="dlry-work-btn">
+            <input
+              type="checkbox"
+              name="office"
+              checked={isOfficeChecked}
+              onChange={handleCheckboxChange}
+             
+            />
+            Office
+          </label>
+        </div>
 
         <Button className="dlry-btn">DELIVER TO THIS ADDRESS</Button>
       </div>
@@ -398,27 +412,22 @@ const RateOptions = (props) => {
 };
 
 const PaymentType = () => {
+  const [formData, setFormData] = useState({
+    cardHolderName: "",
+    cardNumber: "",
+    expirationDate: "",
+    cvv: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
   return (
     <>
-      {/* <div className="del-ct-bg mt-2">
-        <h3 className="hd-tag-font">Payment Type</h3>
-        <AccountCardType
-          cardname="ADD CREDIT / DEBIT CARD"
-          cardimg={creditcard}
-        />
-        <h6 className="opt-dlry-fnt mt-4">OR</h6>
-        <AccountCardType cardname="PAYPAL" cardimg={paypal} />
-
-        <div className="ct-box mt-4">
-          <h6 className="opt-dlry-fnt mt-1">WE ACCEPT :</h6>
-          <img src={visa} alt="visa" className="cr-cd-img" />
-          <img src={mastercard} alt="visa" className="cr-cd-img" />
-          <img src={phonpe} alt="visa" className="cr-cd-img" />
-          <img src={paytm} alt="visa" className="cr-cd-img" />
-          <img src={googlepay} alt="visa" className="cr-cd-img" />
-        </div>
-      </div> */}
-
       <form action="">
         <Accordion className="mt-2">
           <Accordion.Item eventKey="0">
@@ -429,20 +438,46 @@ const PaymentType = () => {
               </div>
             </Accordion.Header>
             <Accordion.Body>
-
-
               <div className="row">
                 <div className="col-md-6">
-                  <input type="text" className="input-box-bdr mt-2 w-100" placeholder="Card Holder Name" />
+                  <input
+                    type="text"
+                    className="input-box-bdr mt-2 w-100"
+                    placeholder="Card Holder Name"
+                    name="cardHolderName"
+                    value={formData.cardHolderName}
+                    onChange={handleInputChange}
+                  />
                 </div>
                 <div className="col-md-6">
-                  <input type="number" className="input-box-bdr mt-2 w-100" placeholder="Card Number" />
+                  <input
+                    type="number"
+                    className="input-box-bdr mt-2 w-100"
+                    placeholder="Card Number"
+                    name="cardNumber"
+                    value={formData.cardNumber}
+                    onChange={handleInputChange}
+                  />
                 </div>
                 <div className="col-md-6">
-                  <input type="date" className="input-box-bdr mt-2 w-100" placeholder="Expiration Date" />
+                  <input
+                    type="date"
+                    className="input-box-bdr mt-2 w-100"
+                    placeholder="Expiration Date"
+                    name="expirationDate"
+                    value={formData.expirationDate}
+                    onChange={handleInputChange}
+                  />
                 </div>
                 <div className="col-md-6">
-                  <input type="text" className="input-box-bdr mt-2 w-100" placeholder="CVV" />
+                  <input
+                    type="text"
+                    className="input-box-bdr mt-2 w-100"
+                    placeholder="CVV"
+                    name="cvv"
+                    value={formData.cvv}
+                    onChange={handleInputChange}
+                  />
                 </div>
               </div>
             </Accordion.Body>
@@ -456,10 +491,13 @@ const PaymentType = () => {
             </Accordion.Header>
             <Accordion.Body>
               <div className="d-flex">
-                <input type="text" className="input-box-bdr" placeholder="Enter UPI ID" />
+                <input
+                  type="text"
+                  className="input-box-bdr"
+                  placeholder="Enter UPI ID"
+                />
                 <button className="check-verify-btn">Verify</button>
               </div>
-
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
@@ -471,10 +509,13 @@ const PaymentType = () => {
             </Accordion.Header>
             <Accordion.Body>
               <div className="d-flex">
-                <input type="text" className="input-box-bdr" placeholder="Enter Paypal ID" />
+                <input
+                  type="text"
+                  className="input-box-bdr"
+                  placeholder="Enter Paypal ID"
+                />
                 <button className="check-verify-btn">Verify</button>
               </div>
-
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
@@ -500,7 +541,7 @@ const CheckoutItem = () => {
   const [deliveryCharges, setDeliveryCharges] = useState(150);
   const [proDetails, setProDetails] = useState([]);
   const [isUpdate, setIsUpdate] = useState(false);
-  const [quantity, setQuantity] = useState('')
+  const [quantity, setQuantity] = useState("");
 
   const { name, price } = useParams();
   const navigate = useNavigate();
@@ -523,33 +564,40 @@ const CheckoutItem = () => {
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
     let total = 0;
     let discountTotal = 0;
-    let quanti =[]
-    setTotal(0)
-    setDisTotal(0)
-    setQuantity('')
+    let quanti = [];
+    setTotal(0);
+    setDisTotal(0);
+    setQuantity("");
     cart.forEach((cartItem) => {
       // Find the corresponding item in item1 array based on the cart item productId
       const matchingItem = item1.find((item) => item.id === cartItem.productId);
 
-      if (matchingItem && matchingItem.images.length > 0 && matchingItem.images[0].productVariantEntities.length > 0) {
-        const productPrice = parseInt(matchingItem.images[0].productVariantEntities[0].price);
-        const productManualPrice = parseInt(matchingItem.images[0].productVariantEntities[0].manualPrice);
+      if (
+        matchingItem &&
+        matchingItem.images.length > 0 &&
+        matchingItem.images[0].productVariantEntities.length > 0
+      ) {
+        const productPrice = parseInt(
+          matchingItem.images[0].productVariantEntities[0].price
+        );
+        const productManualPrice = parseInt(
+          matchingItem.images[0].productVariantEntities[0].manualPrice
+        );
 
         // Calculate the total and discount total based on the cart item's quantity
-        total += productPrice * (cartItem.quantity-1);
-        discountTotal += productManualPrice * (cartItem.quantity-1);
-        quanti.push(cartItem.quantity)
+        total += productPrice * (cartItem.quantity - 1);
+        discountTotal += productManualPrice * (cartItem.quantity - 1);
+        quanti.push(cartItem.quantity);
       }
     });
 
     await setTotal(total);
     await setDisTotal(discountTotal);
-    setQuantity(quanti)
+    setQuantity(quanti);
   }
 
   // Example usage:
   // handleAllPrice(item1); // Call this function to update the total and discount total based on the cart
-
 
   useEffect(() => {
     cart = JSON.parse(localStorage.getItem("cart")) || [];
@@ -575,7 +623,7 @@ const CheckoutItem = () => {
         </div>
         <hr />
         {proDetails.length > 0 &&
-          proDetails.map((item,index) => {
+          proDetails.map((item, index) => {
             // console.log(item)
             return (
               <div className="ct-box mt-4" key={item.id}>
@@ -598,7 +646,7 @@ const CheckoutItem = () => {
                       item.images[0].productVariantEntities.length > 0 &&
                       item.images[0].productVariantEntities[0].size}
                   </h2>
-                  <h2 className="opt-dlry-fnt">Qty : {quantity[index]-1}</h2>
+                  <h2 className="opt-dlry-fnt">Qty : {quantity[index] - 1}</h2>
                 </div>
               </div>
             );
