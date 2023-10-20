b8c45fb1c641159ee6660f1afeb9eace5955f9fd
Alok Sahu <tusharsahu2427@gmail.com>
2023-10-20 18:18:03 +0530

    v17.4

--- src/jsx/desktop-dropdown/DeskDropdown.jsx
+++ src/jsx/desktop-dropdown/DeskDropdown.jsx
@@ -1,26 +1,55 @@
-import React, { useState,useEffect,memo } from "react";
+import React, { useState, useEffect, memo } from "react";
 import "./dropdown-menu.css";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import editorimg from "../../assets/images/earring.jpg";
+import axios from "axios";
 
+const url = "https://api.diwamjewels.com/DMJ/";
+const endPoint = "api/v1/category/maincategoryName/";
+const subSubEndPoint = 'api/v1/category/subcategory/';
 
+const DeskDropdown = ({ cateData }) => {
 
-const DeskDropdown = () => {
   return (
     <>
-      <NewDropdownMenu />
+      <NewDropdownMenu cateData={cateData} />
     </>
   );
 };
 
 export default DeskDropdown;
 
-const NewDropdownMenu = () => {
+const NewDropdownMenu = ({ cateData }) => {
   const [activeCategory, setActiveCategory] = useState("Faishon Jewellery");
+  const [subCateData, setSubCateData] = useState([])
 
-  const handleCategoryMouseOver = (category) => {
+
+  async function getSubSubCate(value) {
+    // console.log(value)
+    try {
+      const res = await axios.get(url + subSubEndPoint + value)
+      // console.log(res.data.data)
+      if (!res.data.data) {
+        setSubCateData([])
+      }
+      else {
+        setSubCateData(res.data.data)
+
+      }
+
+    }
+    catch (err) {
+      console.log(err)
+      setSubCateData([])
+    }
+  }
+
+
+  const handleCategoryMouseOver = async (category, id) => {
     setActiveCategory(category);
+    getSubSubCate(id)
+
   };
 
   const handleCategoryMouseOut = () => {
@@ -32,34 +61,50 @@ const NewDropdownMenu = () => {
       <div className="new-menu-boxvw">
         <div className="d-flex">
           <div className="" style={{ width: "350px" }}>
-            <p className="cate-hd-fntsz">
-              All Jewellery Categories <ArrowRightAltIcon />
-            </p>
-            <MainCategory
-              category="Faishon Jewellery"
-              isActive={activeCategory === "Faishon Jewellery"}
-              onMouseOver={() => handleCategoryMouseOver("Faishon Jewellery")}
-            />
-            <MainCategory
-              category="Gold Jewellery"
-              isActive={activeCategory === "Gold Jewellery"}
-              onMouseOver={() => handleCategoryMouseOver("Gold Jewellery")}
-            />
-            <MainCategory
-              category="Silver Jewellery"
-              isActive={activeCategory === "Silver Jewellery"}
-              onMouseOver={() => handleCategoryMouseOver("Silver Jewellery")}
-            />
+            {/* <p className="cate-hd-fntsz">
+              {/* All Jewellery Categories <ArrowRightAltIcon /> 
+            </p> */}
+
+            {
+              cateData.length > 0 && cateData.map((item) => {
+
+                return (
+                  <>
+                    <MainCategory
+                      category={item.type}
+                      isActive={activeCategory === item.type}
+                      onMouseOver={() => handleCategoryMouseOver(item.type, item.id)}
+                    />
+                  </>
+                )
+              })
+            }
+
+
           </div>
 
           <div className="linefrleft"></div>
-          {(activeCategory === "Faishon Jewellery" ||
-            activeCategory === "Gold Jewellery" ||
-            activeCategory === "Silver Jewellery") && (
+          {(
             <div className="row" style={{ width: "1000px" }}>
-              <SubcategoryBox submaincategory={activeCategory} />
-              <SubcategoryBox submaincategory={activeCategory} />
-              <SubcategoryBox submaincategory={activeCategory} />
+              <div className="col">
+                {/* <p className="main-cate-fnt-sz">{props.submaincategory}</p>
+         */}
+
+
+
+                {
+                  subCateData.length > 0 && subCateData.map((cateList) => {
+                    // console.log(cateList)
+                    return (
+                      <>
+                        <SubcategoryList subcategory={cateList.type} />
+
+                      </>
+                    )
+                  })
+                }
+              </div>
+
               <EditorPick />
             </div>
           )}
@@ -84,15 +129,11 @@ const MainCategory = (props) => {
   );
 };
 
-const SubcategoryBox = (props) => {
+const SubcategoryBox = ({ submaincategory }) => {
+
   return (
     <>
-      <div className="col">
-        <p className="main-cate-fnt-sz">{props.submaincategory}</p>
-        <SubcategoryList subcategory="Bridal Churas" />
-        <SubcategoryList subcategory="Kadas" />
-        <SubcategoryList subcategory="Kaleeras" />
-      </div>
+
     </>
   );
 };
